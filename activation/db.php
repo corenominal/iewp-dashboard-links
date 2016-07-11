206071e19edc4328d454b3d141ab41ae510b1e2b
Philip Newborough <corenominal@corenominal.org>
2016-07-11 18:51:17 +0100

    Plugin activation - set-up DB table

---
+++ activation/db.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Set-up database tables.
+ */
+function iewp_dashboard_links_create_tables()
+{
+	global $wpdb;
+
+	$sql = "CREATE TABLE IF NOT EXISTS `iewp_dashboard_links` (
+			  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+			  `name` varchar(255) NOT NULL DEFAULT '',
+			  `link` varchar(255) NOT NULL DEFAULT '',
+			  PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+
+	$query = $wpdb->query( $sql );
+
+}
+
+iewp_dashboard_links_create_tables();
--- iewp-dashboard-links.php
+++ iewp-dashboard-links.php
@@ -7,3 +7,12 @@
  * Version: 0.0.1
  * Author URI: https://corenominal.org
  */
+
+ /**
+  * Plugin activation functions
+  */
+ function iewp_dashboard_links_activate()
+ {
+ 	require_once( plugin_dir_path( __FILE__ ) . 'activation/db.php' );
+ }
+ register_activation_hook( __FILE__, 'iewp_dashboard_links_activate' );
