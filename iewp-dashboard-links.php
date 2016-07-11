65d9178b4fa728f9e2044c8042d91f4cd060b31f
Philip Newborough <corenominal@corenominal.org>
2016-07-11 18:35:54 +0100

    Initial plugin file

---
+++ iewp-dashboard-links.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: IEWP Dashboard Links
+ * Plugin URI: https://github.com/corenominal/iewp-dashboard-links
+ * Description: A simple WordPress plugin to provide links within the dashboard.
+ * Author: Philip Newborough
+ * Version: 0.0.1
+ * Author URI: https://corenominal.org
+ */
