b37bea48415166b10317420b9c8538cbfd7faf45
Philip Newborough <corenominal@corenominal.org>
2016-07-14 21:39:12 +0100

    Enqueue JS and CSS for dashboard widget

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -13,6 +13,26 @@ function iewp_dashboard_links_widget()
 add_action( 'wp_dashboard_setup', 'iewp_dashboard_links_widget' );
 
 /**
+ * Enqueue additional JavaScript and CSS
+ */
+function iewp_dashboard_links_enqueue_scripts( $hook )
+{
+
+	if( 'index.php' != $hook )
+	{
+		return;
+	}
+
+    wp_register_style( 'iewp_dashboard_links_css', plugin_dir_url( __FILE__ ) . 'css/iewp_dashboard_links.css', array(), '0.0.1', 'all' );
+	wp_enqueue_style( 'iewp_dashboard_links_css' );
+
+	wp_register_script( 'iewp_dashboard_links_js', plugin_dir_url( __FILE__ ) . 'js/iewp_dashboard_links.js', array('jquery'), '0.0.1', true );
+	wp_enqueue_script( 'iewp_dashboard_links_js' );
+
+}
+add_action( 'admin_enqueue_scripts', 'iewp_dashboard_links_enqueue_scripts' );
+
+/**
 * Widget content
 */
 function iewp_dashboard_links_widget_function()
